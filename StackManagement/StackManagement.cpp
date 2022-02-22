683003979fb6c78e69febde8f62dcfd05cbb381e
Omar Núñez <omarsnunez@hotmail.com>
2022-02-22 09:04:40 -0400

    Add project files.

---
+++ StackManagement/StackManagement.cpp
@@ -0,0 +1,32 @@
+// IDS344 - 2022-01 - Grupo 1 - StackManagement
+// Nikita Kravchenko - 1101607
+// Omar Núñez - 1101587
+// Óliver Infante - 1100292
+// Lervis Pinales - 1096323
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int menuOption;
+    
+
+    cout << "Stack Management\n\n";
+    cout << "Welcome.\n";
+    cout << "Select an option. (1 - 4)\n";
+    cout << "1. Add\n";
+    cout << "2. Remove\n";
+    cout << "3. Deploy\n";
+    cout << "4. Exit\n";
+
+    cin >> menuOption;
+    switch (menuOption)
+    {
+        case 1:
+            break;
+        default:
+            break;
+    }
+}
